60c601f3ccb21d92741f659b2103feb6f37d4fbe
Shyy23 <syahrulhidayatulloh14@gmail.com>
2025-02-11 15:18:19 +0700

    footer 1.2

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,230 @@
+<footer
+    id="footer"
+    class="footer mt-auto bg-[--container-clr] px-[5rem] text-[--text-clr]"
+>
+    <div
+        id="footer-top "
+        class="grid grid-cols-12 gap-4 border-b-2 border-[--border-2-clr] py-6"
+    >
+        <div id="footer-top-left " class="col-span-3 p-4">
+            <div class="title mb-3">
+                <h3
+                    class="font-audioWide text-[1.25rem] font-bold text-[--primary-clr]"
+                >
+                    Inventory Barang
+                </h3>
+            </div>
+            <div class="desc text-justify text-[.5rem] font-medium leading-4">
+                <p>
+                    Kami menyediakan layanan
+                    <span class="text-[--primary-clr]">
+                        <em>Inventory</em>
+                        Barang
+                    </span>
+                    di jurusan
+                    <span class="text-[--primary-clr]">
+                        <abbr
+                            title="Pengembangan Perangkat Lunak & Gim"
+                            class="border-none"
+                        >
+                            PPLG
+                        </abbr>
+                    </span>
+                    dengan tujuan mempermudah pengelolaan aset secara efisien
+                    dan transparan.
+                </p>
+                <p>
+                    Melalui sistem yang kami kembangkan, kami berharap dapat
+                    mendukung kelancaran operasional serta meningkatkan
+                    akuntabilitas dalam penggunaan barang.
+                </p>
+            </div>
+        </div>
+        <div
+            id="footer-top-right"
+            class="col-span-8 grid grid-cols-1 gap-4 p-4 md:grid-cols-12"
+        >
+            <!-- INFORMASI -->
+            <div class="info__inventory col-span-12 md:col-span-4">
+                <div class="title mb-3">
+                    <h3 class="text-[1rem] font-bold text-[--primary-clr]">
+                        INFORMASI
+                    </h3>
+                </div>
+                <ul class="space-y-1">
+                    <li>
+                        <a
+                            href="#"
+                            class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                        >
+                            Periksa Barang
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                        >
+                            Status Stock Barang
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                        >
+                            Kategori Barang
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                        >
+                            Kondisi Barang
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                        >
+                            Riwayat Peminjaman
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- TOOLS -->
+            <div class="info__tool col-span-12 md:col-span-4">
+                <div class="title mb-3">
+                    <h3 class="text-[1rem] font-bold text-[--primary-clr]">
+                        TOOLS
+                    </h3>
+                </div>
+                <ul class="space-y-1">
+                    <li>
+                        <a
+                            href="#"
+                            class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                        >
+                            Tambah Stock
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                        >
+                            Update Status
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                        >
+                            Generate Laporan
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                        >
+                            Manajemen User
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- PERUSAHAAN & BANTUAN -->
+            <div class="info__lainnya col-span-12 space-y-6 md:col-span-4">
+                <!-- PERUSAHAAN -->
+                <div class="company">
+                    <div class="title mb-3">
+                        <h3 class="text-[1rem] font-bold text-[--primary-clr]">
+                            SMKN 4 PADALARANG
+                        </h3>
+                    </div>
+                    <div class="text-[.5rem] leading-4 text-[--text-2-clr]">
+                        <p>Jl. Raya Padalarang No.451.</p>
+                        <p>
+                            Kertajaya, Kec. Padalarang, Kabupaten Bandung Barat
+                        </p>
+                        <p>Jawa Barat 40553</p>
+                        <p class="mt-2">
+                            <span class="font-semibold">Jurusan RPL:</span>
+                            <br />
+                            Rekayasa Perangkat Lunak - Pengembangan sistem
+                            informasi dan aplikasi
+                        </p>
+                    </div>
+                </div>
+
+                <!-- BANTUAN -->
+                <div class="help">
+                    <div class="title mb-3">
+                        <h3 class="text-[1rem] font-bold text-[--primary-clr]">
+                            BANTUAN
+                        </h3>
+                    </div>
+                    <ul class="leading-4">
+                        <li>
+                            <a
+                                href="#"
+                                class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                            >
+                                Laporkan Bug
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                            >
+                                Panduan Penggunaan
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                class="block text-[.5rem] font-medium text-[--text-2-clr] transition hover:text-[--primary-clr]"
+                            >
+                                FAQ
+                            </a>
+                        </li>
+                        <li>
+                            <div class="mt-2 text-[.5rem] text-[--text-2-clr]">
+                                <p>Layanan Service:</p>
+                                <p class="font-semibold">
+                                    inventory@smkn4pdl.sch.id
+                                </p>
+                                <p>(022) 6865XXX</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer-bottom" class="grid grid-cols-12 py-6">
+        <div id="copyright-group" class="col-span-8 grid grid-cols-[auto_1fr]">
+            <h3 class="font-audioWide text-[1.25rem] font-bold">Shyy</h3>
+            <p class="self-end pl-4 text-xs font-medium">
+                Copyright © 2025 Syahrul Hidayatulloh. All Right Reserved
+            </p>
+        </div>
+        <div id="social-brands" class="col-span-4 flex gap-4 justify-self-end">
+            <a href="#" class="flex items-center justify-center">
+                <i class="fa-brands fa-whatsapp text-[1rem]"></i>
+            </a>
+            <a href="#" class="flex items-center justify-center">
+                <i class="fa-brands fa-instagram text-[1rem]"></i>
+            </a>
+            <a href="#" class="flex items-center justify-center">
+                <i class="fa-brands fa-github text-[1rem]"></i>
+            </a>
+        </div>
+    </div>
+</footer>
